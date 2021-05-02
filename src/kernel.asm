d40b32f4d023da473be5bdecd80523d06c0d665c
XCaminhante <caminhante@i2pmail.org>
2021-05-02 17:17:06 -0300

    Mais comentários

--- src/kernel.asm
+++ src/kernel.asm
@@ -4,19 +4,25 @@
 ;@@language assembly_x86
 ;@@tabwidth -2
 ;@+others
-;@+node:caminhante.20210502144450.1: ** structs
+;@+node:caminhante.20210502144450.1: ** tipos de dados
 ;@+node:caminhante.20210502144658.1: *3* Multiboot
 multiboot:
   .header_magic = 0x1BADB002
   .flags:
-    .page_aligned = (1 shr 0)
-    .aout_kludge =  (1 shr 16)
+    .page_aligned_modules = (1 shl 0)
+    .prefered_graphics_mode = (1 shl 1)
+    .alternative_binary_format =  (1 shl 15)
+  .graphics_mode:
+    .linear = 0
+    .EGA_text = 1
+;@+node:caminhante.20210502170507.1: *4* struc multiboot_header_magic _flags
 struc multiboot_header_magic _flags {
   align 4
   .magic dd multiboot.header_magic
   .flags dd _flags
   .checksum dd -(multiboot.header_magic + _flags)
 }
+;@+node:caminhante.20210502170515.1: *4* struc multiboot_header_addr _header, _load, _load_end, _bss_end, _entry
 struc multiboot_header_addr _header, _load, _load_end, _bss_end, _entry {
   ; deve apontar para multiboot_header_magic
   .header_addr    dd _header
@@ -32,10 +38,15 @@ struc multiboot_header_addr _header, _load, _load_end, _bss_end, _entry {
   ; endereço físico de salto para o código do kernel
   .entry_addr     dd _entry
 }
+;@+node:caminhante.20210502170522.1: *4* struc multiboot_header_graphics
 struc multiboot_header_graphics {
+  ; 0 = modo linear (pixels), 1 = modo de texto EGA
   .mode_type  rd 1
+  ; largura sugerida em colunas
   .width      rd 1
+  ; altura sugerida em linhas
   .height     rd 1
+  ; profundidade de bits
   .depth      rd 1
 }
 ;@+node:caminhante.20210502144350.1: ** ELF
